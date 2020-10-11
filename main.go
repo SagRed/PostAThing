b1fe87b178a77e3d346a4c3b7bed854f0d4ade6d
sagred <sagred17@gmail.com>
2020-10-11 22:47:53 +0530

    added redis

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 )
 
 var templates *template.Template
+var client *redis.Client
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home).Methods("GET")
 	http.Handle("/", r)
@@ -19,6 +25,10 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Started home")
-	templates.ExecuteTemplate(w, "index.html", nil)
+	data, err := client.Get(client.Context(), "data").Result()
+	if err != nil {
+		return
+	}
+	fmt.Println("Getting data...")
+	templates.ExecuteTemplate(w, "index.html", data)
 }
